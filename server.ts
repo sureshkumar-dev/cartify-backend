08409ae2ac8826937cb51ec242754073e3a315ed
sureshkumar-dev <sureshpackirisamy05@gmail.com>
2026-08-12 19:22:05 +0530

    fixed core issue

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-import  { Request , Response } from "express";
+import { Request, Response } from "express";
 import cors from 'cors'
 import express from 'express'
 import router from "./routes/UserRoutes";
@@ -7,14 +7,22 @@ import db from "./config/db";
 import userRouter from "./routes/UserRoutes";
 import sellerRouter from "./routes/SellerRoutes";
 import adminRouter from './routes/AdminRoute'
-import { encrypt , decrypt } from "./controllers/userController";
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://cartify-ecommerce-marketplace.vercel.app"
-    ],
-    credentials: true
-}));
+import { encrypt, decrypt } from "./controllers/userController";
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://cartify-ecommerce-marketplace.vercel.app",
+];
+
+app.use(
+    cors({
+        origin: allowedOrigins,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.options("*", cors());
 app.use(express.json())
 app.use("/uploads", express.static("uploads"));
 db.query("SELECT * FROM users", (err, result) => {
@@ -25,17 +33,17 @@ db.query("SELECT * FROM users", (err, result) => {
 
 })
 const enc = encrypt("suresh")
-console.log("encrypted",enc);
+console.log("encrypted", enc);
 const dec = decrypt(enc)
-console.log("decrypted",dec);
+console.log("decrypted", dec);
 
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.send("server working")
 })
 app.use("/", userRouter)
-app.use('/',sellerRouter)
-app.use('/',adminRouter)
+app.use('/', sellerRouter)
+app.use('/', adminRouter)
 app.listen(3000, () => {
     console.log('server is started');
 })
