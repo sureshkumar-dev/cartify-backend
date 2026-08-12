20d2449b7ab42f9066b5135b1e25d11f46840b01
sureshkumar-dev <sureshpackirisamy05@gmail.com>
2026-08-12 19:26:09 +0530

    fixed core issue 2

--- server.ts
+++ server.ts
@@ -1,49 +1,40 @@
 import app from "./app";
 import { Request, Response } from "express";
-import cors from 'cors'
-import express from 'express'
-import router from "./routes/UserRoutes";
+import cors from "cors";
+import express from "express";
 import db from "./config/db";
 import userRouter from "./routes/UserRoutes";
 import sellerRouter from "./routes/SellerRoutes";
-import adminRouter from './routes/AdminRoute'
-import { encrypt, decrypt } from "./controllers/userController";
+import adminRouter from "./routes/AdminRoute";
+
 const allowedOrigins = [
-    "http://localhost:5173",
-    "https://cartify-ecommerce-marketplace.vercel.app",
+  "http://localhost:5173",
+  "https://cartify-ecommerce-marketplace.vercel.app",
 ];
 
 app.use(
-    cors({
-        origin: allowedOrigins,
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
-app.options("*", cors());
-app.use(express.json())
+app.use(express.json());
+
 app.use("/uploads", express.static("uploads"));
-db.query("SELECT * FROM users", (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
 
-})
-const enc = encrypt("suresh")
-console.log("encrypted", enc);
-const dec = decrypt(enc)
-console.log("decrypted", dec);
+app.get("/", (req: Request, res: Response) => {
+  res.send("server working");
+});
 
+app.use("/", userRouter);
+app.use("/", sellerRouter);
+app.use("/", adminRouter);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("server working")
-})
-app.use("/", userRouter)
-app.use('/', sellerRouter)
-app.use('/', adminRouter)
-app.listen(3000, () => {
-    console.log('server is started');
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
